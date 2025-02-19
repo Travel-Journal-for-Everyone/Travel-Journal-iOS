33d06569847683b496a94c8a532a08a68563658e
Kimseongmin <sjybext@naver.com>
2025-02-19 17:10:04 +0900

    feat: 공통으로 사용되는 버튼 구현
    - TJButton으로 여러 뷰에서 공통으로 사용되는 버튼 구현
    - 추후 색상 정해지면 색상 정의 해줘야 함

---
+++ TravelJournalForEveryone/TravelJournalForEveryone/Presentation/General/TJButton.swift
@@ -0,0 +1,70 @@
+//
+//  TJButton.swift
+//  TravelJournalForEveryone
+//
+//  Created by 김성민 on 2/19/25.
+//
+
+import SwiftUI
+
+struct TJButton: View {
+    let title: String
+    let action: () -> Void
+    var isDisabled: Bool
+    var size: Size
+    var height: CGFloat
+    
+    init(
+        title: String,
+        action: @escaping () -> Void,
+        isDisabled: Bool = false,
+        size: Size = .long,
+        height: CGFloat = 50
+    ) {
+        self.title = title
+        self.action = action
+        self.isDisabled = isDisabled
+        self.size = size
+        self.height = height
+    }
+    
+    var body: some View {
+        Button {
+            action()
+        } label: {
+            Text(title)
+                .foregroundStyle(.white)
+                .background {
+                    RoundedRectangle(cornerRadius: 8)
+                        .frame(width: size.width, height: height)
+                        // TODO: - 색상 정해주기
+                        .foregroundStyle(isDisabled ? .gray : .blue)
+                }
+        }
+        .disabled(isDisabled)
+    }
+}
+
+#Preview {
+    VStack(spacing: 50) {
+        TJButton(title: "작성 완료", action: { })
+        TJButton(title: "작성 완료", action: { }, isDisabled: true)
+        
+        TJButton(title: "중복확인", action: { }, size: .short)
+        TJButton(title: "중복확인", action: { }, isDisabled: true, size: .short)
+    }
+}
+
+extension TJButton {
+    enum Size {
+        case short
+        case long
+        
+        var width: CGFloat {
+            switch self {
+            case .short: return 81
+            case .long: return 333
+            }
+        }
+    }
+}
