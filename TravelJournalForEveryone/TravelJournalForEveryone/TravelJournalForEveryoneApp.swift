4b120869a321a5795d00abda26c5bf3e75f3152f
Kimseongmin <sjybext@naver.com>
2025-02-19 15:53:58 +0900

    chore: 프로젝트 생성

---
+++ TravelJournalForEveryone/TravelJournalForEveryone/TravelJournalForEveryoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelJournalForEveryoneApp.swift
+//  TravelJournalForEveryone
+//
+//  Created by 김성민 on 2/19/25.
+//
+
+import SwiftUI
+
+@main
+struct TravelJournalForEveryoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
