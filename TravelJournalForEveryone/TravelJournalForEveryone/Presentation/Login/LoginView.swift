50439b86608d638647efa56529660bb7b622af47
Kimseongmin <sjybext@naver.com>
2025-02-19 16:25:43 +0900

    docs: 폴더 및 파일 생성
    - 해당 브랜치에서 필요한 폴더와 파일 초안 생성

---
+++ TravelJournalForEveryone/TravelJournalForEveryone/Presentation/Login/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  TravelJournalForEveryone
+//
+//  Created by 김성민 on 2/19/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("LoginView")
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ TravelJournalForEveryone/TravelJournalForEveryone/Presentation/MyJournal/MyJournalView.swift
@@ -0,0 +1,18 @@
+//
+//  MyJournalView.swift
+//  TravelJournalForEveryone
+//
+//  Created by 김성민 on 2/19/25.
+//
+
+import SwiftUI
+
+struct MyJournalView: View {
+    var body: some View {
+        Text("MyJournalView")
+    }
+}
+
+#Preview {
+    MyJournalView()
+}
