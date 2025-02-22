85c8139b58ccf0af1d04c49c1d3881198a68c9f6
Kimseongmin <sjybext@naver.com>
2025-02-22 22:25:57 +0900

    feat: Pretendard 폰트 추가
    - Font를 확장하여 Pretendard 폰트 사용 방법 설정

---
+++ TravelJournalForEveryone/TravelJournalForEveryone/Presentation/Extensions/Font+Pretendard.swift
@@ -0,0 +1,22 @@
+//
+//  Font+Pretendard.swift
+//  TravelJournalForEveryone
+//
+//  Created by 김성민 on 2/22/25.
+//
+
+import SwiftUI
+
+extension Font {
+    static func pretendardSemiBold(_ size: CGFloat) -> Font {
+        return .custom("Pretendard-SemiBold", size: size)
+    }
+    
+    static func pretendardMedium(_ size: CGFloat) -> Font {
+        return .custom("Pretendard-Medium", size: size)
+    }
+    
+    static func pretendardRegular(_ size: CGFloat) -> Font {
+        return .custom("Pretendard-Regular", size: size)
+    }
+}
