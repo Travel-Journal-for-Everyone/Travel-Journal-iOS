feef856e5e2ff12fc54f738a7b47a1932b23d0f0
Kimseongmin <sjybext@naver.com>
2025-02-24 17:42:40 +0900

    feat: 카카오 로그인을 위한 초기 설정

--- TravelJournalForEveryone/TravelJournalForEveryone/Application/TravelJournalForEveryoneApp.swift
+++ TravelJournalForEveryone/TravelJournalForEveryone/Application/TravelJournalForEveryoneApp.swift
@@ -6,12 +6,25 @@
 //
 
 import SwiftUI
+import KakaoSDKCommon
+import KakaoSDKAuth
+import KakaoSDKUser
 
 @main
 struct TravelJournalForEveryoneApp: App {
+    init() {
+        // kakao SDK 초기화
+        KakaoSDK.initSDK(appKey: Bundle.main.kakaoNativeAppKey)
+    }
+    
     var body: some Scene {
         WindowGroup {
             AuthenticationView(authViewModel: AuthenticationViewModel())
+                .onOpenURL { url in
+                    if (AuthApi.isKakaoTalkLoginUrl(url)) {
+                        _ = AuthController.handleOpenUrl(url: url)
+                    }
+                }
         }
     }
 }
