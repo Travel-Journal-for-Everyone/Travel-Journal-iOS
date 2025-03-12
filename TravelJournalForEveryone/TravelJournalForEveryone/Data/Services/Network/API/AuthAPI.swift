4270c7ab7f1ed80bc89dcbae65eb153202d919c4
Kimseongmin <sjybext@naver.com>
2025-03-13 00:44:41 +0900

    [TJFE-94] feat: API 요청에 필요한 Endpoint, Request, Response 파일 작성
    - 백엔드 서버에 JWT 토큰을 요청하는 API

---
+++ TravelJournalForEveryone/TravelJournalForEveryone/Data/Services/Network/API/AuthAPI.swift
@@ -0,0 +1,59 @@
+//
+//  AuthAPI.swift
+//  TravelJournalForEveryone
+//
+//  Created by 김성민 on 3/12/25.
+//
+
+import Foundation
+import Alamofire
+
+enum AuthAPI {
+    case fetchJWTToken(FetchJWTTokenRequest)
+}
+
+extension AuthAPI: EndPoint {
+    var basePath: String { "/v1/auth" }
+    
+    var path: String {
+        switch self {
+        case .fetchJWTToken(let request): "/login/\(request.loginProvider)/id-token"
+        }
+    }
+    
+    var queryParameters: [String : String]? {
+        switch self {
+        case .fetchJWTToken: nil
+        }
+    }
+    
+    var method: HTTPMethod {
+        switch self {
+        case .fetchJWTToken: .post
+        }
+    }
+    
+    var headers: HTTPHeaders? {
+        switch self {
+        case .fetchJWTToken(let request): HeaderType.bearer(request.idToken).value
+        }
+    }
+    
+    var parameterEncoding: any ParameterEncoding {
+        switch self {
+        case .fetchJWTToken: URLEncoding.default
+        }
+    }
+    
+    var bodyParameters: Parameters? {
+        switch self {
+        case .fetchJWTToken: nil
+        }
+    }
+    
+    var requiresAuth: Bool {
+        switch self {
+        case .fetchJWTToken: false
+        }
+    }
+}
