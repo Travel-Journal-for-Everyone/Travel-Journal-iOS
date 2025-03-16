7f254b22a17b533f074602a11b82c2931867530f
Kimseongmin <sjybext@naver.com>
2025-03-16 22:48:59 +0900

    [TJFE-50] feat: User Entity 초안 작성
    - 추후에 프로퍼티가 추가될 가능성 높음

---
+++ TravelJournalForEveryone/TravelJournalForEveryone/Domain/Entities/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  TravelJournalForEveryone
+//
+//  Created by 김성민 on 3/15/25.
+//
+
+import Foundation
+
+struct User {
+    // let profileImageURLString: String
+    let nickname: String
+    let accountScope: ProfileVisibilityScope
+    let isFirstLogin: Bool
+}
