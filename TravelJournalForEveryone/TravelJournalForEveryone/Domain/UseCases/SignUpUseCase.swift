f9c2db54feb7b70a82ff91d4b29085303c27211c
Kimseongmin <sjybext@naver.com>
2025-03-24 16:51:52 +0900

    [TJFE-49] feat: SignUpUseCase에 fetchUser 기능 추가
    - fetchUser 메서드 추가
    - saveUser 메서드 추가

--- TravelJournalForEveryone/TravelJournalForEveryone/Domain/UseCases/SignUpUseCase.swift
+++ TravelJournalForEveryone/TravelJournalForEveryone/Domain/UseCases/SignUpUseCase.swift
@@ -9,10 +9,10 @@ import Foundation
 import Combine
 
 protocol SignUpUseCase {
-    func execute(
+    @MainActor func execute(
         nickname: String,
         accountScope: AccountScope
-    ) -> AnyPublisher<Bool, NetworkError>
+    ) -> AnyPublisher<Bool, Error>
 }
 
 struct DefaultLoginCompleteUseCase: SignUpUseCase {
@@ -22,13 +22,47 @@ struct DefaultLoginCompleteUseCase: SignUpUseCase {
         self.userRepository = userRepository
     }
     
+    @MainActor
     func execute(
         nickname: String,
         accountScope: AccountScope
-    ) -> AnyPublisher<Bool, NetworkError> {
+    ) -> AnyPublisher<Bool, Error> {
         return userRepository.completeSignUp(
             nickname: nickname,
             accountScope: accountScope
+        )
+        .mapError { $0 as Error }
+        .flatMap { isSuccess in
+            if isSuccess {
+                return fetchUser()
+                    .map { user in
+                        saveUser(user)
+                        return true
+                    }
+                    .catch { error in
+                        Fail(error: error)
+                    }
+                    .eraseToAnyPublisher()
+            } else {
+                return Just(false)
+                    .setFailureType(to: Error.self)
+                    .eraseToAnyPublisher()
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    private func fetchUser() -> AnyPublisher<User, Error> {
+        let memberID = UserDefaults.standard.integer(forKey: UserDefaultsKey.memberID.value)
+        
+        return userRepository.fetchUser(memberID: memberID)
+    }
+    
+    @MainActor
+    private func saveUser(_ user: User) {
+        DIContainer.shared.userInfoManager.updateUser(
+            nickname: user.nickname,
+            accountScope: user.accountScope
         )
     }
 }
