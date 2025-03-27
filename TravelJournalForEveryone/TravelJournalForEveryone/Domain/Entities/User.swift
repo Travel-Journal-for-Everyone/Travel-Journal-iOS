0952e38a54efefffd18b88eb284028af4c4f83bf
Kimseongmin <sjybext@naver.com>
2025-03-27 23:51:17 +0900

    feat: User 엔터티 추가 정의

--- TravelJournalForEveryone/TravelJournalForEveryone/Domain/Entities/User.swift
+++ TravelJournalForEveryone/TravelJournalForEveryone/Domain/Entities/User.swift
@@ -8,8 +8,29 @@
 import Foundation
 
 struct User {
-    // let profileImageURLString: String
     let nickname: String
+    let profileImageURLString: String
     let accountScope: AccountScope
+    let followingCount: Int
+    let followerCount: Int
+    let travelJournalCount: Int
+    let placesCount: Int
     let isFirstLogin: Bool
+    let regionDatas: [RegionData]
+}
+
+extension User {
+    static func mock() -> User {
+        return User(
+            nickname: "마루김마루",
+            profileImageURLString: "",
+            accountScope: .privateProfile,
+            followingCount: 1,
+            followerCount: 99,
+            travelJournalCount: 15,
+            placesCount: 25,
+            isFirstLogin: false,
+            regionDatas: [.mock()]
+        )
+    }
 }
