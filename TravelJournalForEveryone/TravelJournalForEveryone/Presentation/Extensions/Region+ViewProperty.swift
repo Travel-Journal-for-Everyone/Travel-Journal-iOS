38f59ec87938d82a4659032d5d2bc6504f752d29
Kimseongmin <sjybext@naver.com>
2025-04-01 11:31:22 +0900

    [TJFE-110] feat: 지도 뷰 UI 구현

--- TravelJournalForEveryone/TravelJournalForEveryone/Presentation/Extensions/Region+ViewProperty.swift
+++ TravelJournalForEveryone/TravelJournalForEveryone/Presentation/Extensions/Region+ViewProperty.swift
@@ -1,5 +1,5 @@
 //
-//  Region+ImageResourceString.swift
+//  Region+ViewProperty.swift
 //  TravelJournalForEveryone
 //
 //  Created by 김성민 on 4/1/25.
@@ -16,6 +16,17 @@ extension Region {
         case .gyeongsang: "gyeongsang"
         case .jeolla: "jeolla"
         case .jeju: "jeju"
+        }
+    }
+    
+    var mapTitle: String {
+        switch self {
+        case .metropolitan: "서울 · 경기 · 인천"
+        case .gangwon: "강원도"
+        case .chungcheong: "충청도"
+        case .gyeongsang: "경상도"
+        case .jeolla: "전라도"
+        case .jeju: "제주도"
         }
     }
 }
