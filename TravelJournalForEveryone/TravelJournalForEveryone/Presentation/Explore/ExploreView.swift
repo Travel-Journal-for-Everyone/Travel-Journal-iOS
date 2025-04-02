77abf08bd471785d3274794bbb1a9ccca43e9fa5
Kimseongmin <sjybext@naver.com>
2025-04-02 13:42:30 +0900

    feat: 탐험하기, 검색하기 임시 뷰 추가

---
+++ TravelJournalForEveryone/TravelJournalForEveryone/Presentation/Explore/ExploreView.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreView.swift
+//  TravelJournalForEveryone
+//
+//  Created by 김성민 on 4/2/25.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text("탐험하기")
+    }
+}
+
+#Preview {
+    ExploreView()
+}
