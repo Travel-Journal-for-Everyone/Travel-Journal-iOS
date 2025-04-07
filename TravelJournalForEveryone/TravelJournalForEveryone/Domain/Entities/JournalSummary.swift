d0d50ee7fa0f96d852eb0d07644c8a6b9823f9e8
Kimseongmin <sjybext@naver.com>
2025-04-08 00:46:31 +0900

    feat: JournalSummary 도메인 엔터티 작성

---
+++ TravelJournalForEveryone/TravelJournalForEveryone/Domain/Entities/JournalSummary.swift
@@ -0,0 +1,30 @@
+//
+//  JournalSummary.swift
+//  TravelJournalForEveryone
+//
+//  Created by 김성민 on 4/8/25.
+//
+
+import Foundation
+
+struct JournalSummary {
+    let hashTag: [String]
+    let title: String
+    let nights: Int
+    let days: Int
+    let startDateString: String
+    let endDateString: String
+}
+
+extension JournalSummary {
+    static func mock(title: String) -> Self {
+        return .init(
+            hashTag: ["부산", "해변", "해안욕장"],
+            title: title,
+            nights: 2,
+            days: 3,
+            startDateString: "2025.04.07",
+            endDateString: "2025.04.09"
+        )
+    }
+}
