0fef469ab1fef88792cb8c3feb68cbc5bcfb5ca2
Kimseongmin <sjybext@naver.com>
2025-04-12 17:46:03 +0900

    feat: FetchJournals Endpoint 작성

--- TravelJournalForEveryone/TravelJournalForEveryone/Data/Network/API/MembersAPI.swift
+++ TravelJournalForEveryone/TravelJournalForEveryone/Data/Network/API/MembersAPI.swift
@@ -10,6 +10,7 @@ import Alamofire
 
 enum MembersAPI {
     case fetchUser(memberID: Int)
+    case fetchJournalList(FetchJournalsRequest)
 }
 
 extension MembersAPI: EndPoint {
@@ -21,6 +22,8 @@ extension MembersAPI: EndPoint {
         switch self {
         case .fetchUser(let memberID):
             return "/\(memberID)"
+        case .fetchJournalList(let request):
+            return "/\(request.memberID)/journals/region/\(request.regionName)"
         }
     }
     
@@ -28,40 +31,45 @@ extension MembersAPI: EndPoint {
         switch self {
         case .fetchUser:
             return nil
+        case .fetchJournalList(let request):
+            return [
+                "page": "\(request.pageNumber)",
+                "size": "\(request.pageSize)"
+            ]
         }
     }
     
     var method: HTTPMethod {
         switch self {
-        case .fetchUser:
+        case .fetchUser, .fetchJournalList:
             return .get
         }
     }
     
     var headers: HTTPHeaders? {
         switch self {
-        case .fetchUser:
+        case .fetchUser, .fetchJournalList:
             return nil
         }
     }
     
     var parameterEncoding: ParameterEncoding {
         switch self {
-        case .fetchUser:
+        case .fetchUser, .fetchJournalList:
             return URLEncoding.default
         }
     }
     
     var bodyParameters: Parameters? {
         switch self {
-        case .fetchUser:
+        case .fetchUser, .fetchJournalList:
             return nil
         }
     }
     
     var requiresAuth: Bool {
         switch self {
-        case .fetchUser:
+        case .fetchUser, .fetchJournalList:
             return true
         }
     }
