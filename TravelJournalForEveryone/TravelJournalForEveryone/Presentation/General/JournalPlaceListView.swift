b12c3c80aaa055c3d884a7603f9cfa1e50e00dc8
Kimseongmin <sjybext@naver.com>
2025-04-12 19:39:27 +0900

    feat: FetchJournals ViewModels 반영
    - Pagination 구현

--- TravelJournalForEveryone/TravelJournalForEveryone/Presentation/General/JournalPlaceListView.swift
+++ TravelJournalForEveryone/TravelJournalForEveryone/Presentation/General/JournalPlaceListView.swift
@@ -36,18 +36,21 @@ struct JournalPlaceListView: View {
                 )) {
                     journalListView()
                         .tag(0)
-                        .onAppear {
-                            viewModel.send(.journalListViewOnAppear)
+                        .task {
+                            if viewModel.state.isJournalsInitialLoading {
+                                viewModel.send(.journalListViewOnAppear)
+                            }
                         }
                     
                     placeGridView()
                         .tag(1)
-                        .onAppear {
+                        .task {
                             viewModel.send(.placeGridViewOnAppear)
                         }
                 }
                 .tabViewStyle(.page(indexDisplayMode: .never))
                 .contentMargins(.horizontal, 16)
+                .contentMargins(0, for: .scrollIndicators)
                 .ignoresSafeArea(.all, edges: .bottom)
             case .like:
                 journalListView()
@@ -79,21 +82,31 @@ struct JournalPlaceListView: View {
     
     @ViewBuilder
     private func journalListView() -> some View {
-        if viewModel.state.journalSummaries.isEmpty {
-            // TODO: - EmptyView 구현
-            Text("작성된 여행 일지가 없습니다.")
+        if viewModel.state.isJournalsInitialLoading {
+            ProgressView()
         } else {
-            ScrollView(.vertical) {
-                LazyVStack(spacing: 15) {
-                    Color.clear
-                        .frame(height: 5)
-                    
-                    ForEach(viewModel.state.journalSummaries, id: \.id) { journalSummary in
-                        JournalListCell(journalSummary)
+            if viewModel.state.journalSummaries.isEmpty {
+                // TODO: - EmptyView 구현
+                Text("작성된 여행 일지가 없습니다.")
+            } else {
+                ScrollView(.vertical) {
+                    LazyVStack(spacing: 15) {
+                        Color.clear
+                            .frame(height: 5)
+                        
+                        ForEach(viewModel.state.journalSummaries, id: \.id) { journalSummary in
+                            JournalListCell(journalSummary)
+                        }
+                        
+                        if !viewModel.state.isLastJournalsPage {
+                            ProgressView()
+                                .task {
+                                    viewModel.send(.journalListNextPageOnAppear)
+                                }
+                        }
                     }
                 }
             }
-            .scrollIndicators(.never)
         }
     }
     
@@ -118,7 +131,6 @@ struct JournalPlaceListView: View {
                     }
                 }
             }
-            .scrollIndicators(.never)
         }
     }
 }
--- TravelJournalForEveryone/TravelJournalForEveryone/Presentation/General/JournalPlaceListViewModel.swift
+++ TravelJournalForEveryone/TravelJournalForEveryone/Presentation/General/JournalPlaceListViewModel.swift
@@ -12,7 +12,10 @@ final class JournalPlaceListViewModel: ObservableObject {
     @Published private(set) var state = State()
     
     private let fetchJournalsUseCase: FetchJournalsUseCase
+    
     private let user: User
+    private var regionName: String = ""
+    private var currentJournalsPageNumber: Int = 0
     
     private var cancellables: Set<AnyCancellable> = []
     
@@ -28,12 +31,15 @@ final class JournalPlaceListViewModel: ObservableObject {
         updateSegmentIndex()
         updateNavigationTitle()
         updateSummaryCount()
+        updateRegionName()
     }
     
     func send(_ intent: Intent) {
         switch intent {
         case .journalListViewOnAppear:
-            handleJournalListViewOnAppear()
+            fetchJournals()
+        case .journalListNextPageOnAppear:
+            fetchJournals()
         case .placeGridViewOnAppear:
             handlePlaceGridViewOnAppear()
         case .selectSegment(let index):
@@ -51,31 +57,19 @@ extension JournalPlaceListViewModel {
         var journalSummariesCount: Int = 0
         var placeSummaries: [PlaceSummary] = []
         var placeSummariesCount: Int = 0
+        var isJournalsInitialLoading: Bool = true
+        var isLastJournalsPage: Bool = false
     }
     
     enum Intent {
         case journalListViewOnAppear
+        case journalListNextPageOnAppear
         case placeGridViewOnAppear
         case selectSegment(Int)
     }
 }
 
 extension JournalPlaceListViewModel {
-    private func handleJournalListViewOnAppear() {
-        // TEST - onAppear 될 때마다 API 통신되는 지 추후 확인하기.
-        self.state.journalSummaries = [
-            .mock(id: 0, title: "바다만 주구장창 보았던 부산 여행 🌊"),
-            .mock(id: 1, title: "가을 느낌 한가득! 울산 간월제 등산️ ⛰️"),
-            .mock(id: 2, title: "맛집 한가득 입이 행복했던 대구 😋"),
-            .mock(id: 3, title: "주구장창 보았던 부산 여행 🌊"),
-            .mock(id: 4, title: "느낌 한가득! 울산 간월제 등산️ ⛰️"),
-            .mock(id: 5, title: "한가득 입이 행복했던 대구 😋"),
-            .mock(id: 6, title: "바다만 주구장창 보았던 부산 🌊"),
-            .mock(id: 7, title: "가을 느낌 한가득! 울산 간월제 ⛰️"),
-            .mock(id: 8, title: "맛집 한가득 입이 행복했던 😋"),
-        ]
-    }
-    
     private func handlePlaceGridViewOnAppear() {
         // TEST - onAppear 될 때마다 API 통신되는 지 추후 확인하기.
         self.state.placeSummaries = [
@@ -98,6 +92,40 @@ extension JournalPlaceListViewModel {
         if self.state.viewType == .save {
             updateNavigationTitle()
         }
+    }
+    
+    private func fetchJournals() {
+        guard !self.state.isLastJournalsPage else { return }
+        
+        fetchJournalsUseCase.execute(
+            // TODO: - 임시로 memberID 넣었음
+            memberID: 2,
+            regionName: regionName,
+            pageNumber: currentJournalsPageNumber
+        )
+        .sink { [weak self] completion in
+            guard let self else { return }
+            
+            switch completion {
+            case .finished:
+                self.state.isJournalsInitialLoading = false
+            case .failure(let error):
+                print("⛔️ Fetch Journals Error: \(error)")
+            }
+        } receiveValue: { [weak self] journalsPage in
+            guard let self else { return }
+            
+            if journalsPage.isEmpty {
+                self.state.journalSummaries = []
+            } else {
+                self.state.journalSummaries.append(
+                    contentsOf: journalsPage.journalSummaries
+                )
+                self.state.isLastJournalsPage = journalsPage.isLast
+                self.currentJournalsPageNumber += 1
+            }
+        }
+        .store(in: &cancellables)
     }
     
     private func updateSegmentIndex() {
@@ -142,6 +170,15 @@ extension JournalPlaceListViewModel {
             self.state.placeSummariesCount = self.user.regionDatas[regionIndex].placesCount
         case .save, .like:
             break
+        }
+    }
+    
+    private func updateRegionName() {
+        switch self.state.viewType {
+        case .all, .save, .like:
+            break
+        case .region(let region):
+            self.regionName = region.rawValue
         }
     }
 }
