07d92f7c2c18efe5b16476911e73733888ccfd4f
Kimseongmin <sjybext@naver.com>
2025-04-12 17:46:34 +0900

    feat: FetchJournals 레포지토리 작성

---
+++ TravelJournalForEveryone/TravelJournalForEveryone/Data/Repositories/DefaultJournalRepository.swift
@@ -0,0 +1,38 @@
+//
+//  DefaultJournalRepository.swift
+//  TravelJournalForEveryone
+//
+//  Created by 김성민 on 4/12/25.
+//
+
+import Foundation
+import Combine
+
+final class DefaultJournalRepository: JournalRepository {
+    private let networkService: NetworkService
+    
+    init(networkService: NetworkService) {
+        self.networkService = networkService
+    }
+    
+    func fetchJournals(
+        memberID: Int,
+        regionName: String,
+        pageNumber: Int,
+        pageSize: Int
+    ) -> AnyPublisher<JournalsPage, NetworkError> {
+        let request = FetchJournalsRequest(
+            memberID: memberID,
+            regionName: regionName,
+            pageNumber: pageNumber,
+            pageSize: pageSize
+        )
+        
+        return networkService.request(
+            MembersAPI.fetchJournalList(request),
+            decodingType: FetchJournalsResponseDTO.self
+        )
+        .map { $0.toEntity() }
+        .eraseToAnyPublisher()
+    }
+}
---
+++ TravelJournalForEveryone/TravelJournalForEveryone/Domain/Interfaces/JournalRepository.swift
@@ -0,0 +1,18 @@
+//
+//  JournalRepository.swift
+//  TravelJournalForEveryone
+//
+//  Created by 김성민 on 4/12/25.
+//
+
+import Foundation
+import Combine
+
+protocol JournalRepository {
+    func fetchJournals(
+        memberID: Int,
+        regionName: String,
+        pageNumber: Int,
+        pageSize: Int
+    ) -> AnyPublisher<JournalsPage, NetworkError>
+}
