c2d68b89618e97a6fcc7ae7dbb19ef24ccd111cb
Kimseongmin <sjybext@naver.com>
2025-04-15 17:09:30 +0900

    feat: FetchPlacesResponseDTO 구현

---
+++ TravelJournalForEveryone/TravelJournalForEveryone/Data/Network/DTO/Members/FetchPlacesResponseDTO.swift
@@ -0,0 +1,61 @@
+//
+//  FetchPlacesResponseDTO.swift
+//  TravelJournalForEveryone
+//
+//  Created by 김성민 on 4/15/25.
+//
+
+import Foundation
+
+struct FetchPlacesResponseDTO: Decodable {
+    let content: [PlaceSummaryDTO]
+    let pageable: PageableInfo
+    let totalPages: Int
+    let totalElements: Int
+    let last: Bool
+    let size: Int
+    let number: Int
+    let sort: SortInfo
+    let numberOfElements: Int
+    let first: Bool
+    let empty: Bool
+}
+
+extension FetchPlacesResponseDTO {
+    struct PlaceSummaryDTO: Decodable {
+        let id: Int
+        let thumbnailURLString: String
+        let name: String
+        let address: String
+        
+        private enum CodingKeys: String, CodingKey {
+            case id = "placeId"
+            case thumbnailURLString = "thumbnailUrl"
+            case name = "title"
+            case address = "region"
+        }
+    }
+}
+
+extension FetchPlacesResponseDTO {
+    func toEntity() -> Pageable<PlaceSummary> {
+        return .init(
+            totalJournals: totalElements,
+            isLast: last,
+            pageNumber: number,
+            isEmpty: empty,
+            contents: content.map { $0.toEntity() }
+        )
+    }
+}
+
+extension FetchPlacesResponseDTO.PlaceSummaryDTO {
+    func toEntity() -> PlaceSummary {
+        return .init(
+            id: id,
+            thumbnailURLString: thumbnailURLString,
+            name: name,
+            address: address
+        )
+    }
+}
