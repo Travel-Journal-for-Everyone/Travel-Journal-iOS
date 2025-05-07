800124e3593a1a46725d81ca462ead94268b7c3a
JURI <80569323+juri123123@users.noreply.github.com>
2025-05-07 17:34:45 +0900

    feat: #169 search api 간소화

--- TravelJournalForEveryone/TravelJournalForEveryone/Data/Network/API/SearchAPI.swift
+++ TravelJournalForEveryone/TravelJournalForEveryone/Data/Network/API/SearchAPI.swift
@@ -8,25 +8,43 @@
 import Foundation
 import Alamofire
 
+enum SearchType {
+    case member
+    case place
+    case journal
+    
+    var path: String {
+        switch self {
+        case .member:
+            "/members"
+        case .place:
+            "/palces"
+        case .journal:
+            "/journals"
+        }
+    }
+}
+
 enum SearchAPI {
-    case searchMembers(SearchMembersRequest)
+    case search(type: SearchType, request: SearchRequest)
 }
 
 extension SearchAPI: EndPoint {
+    
     var basePath: String {
         return "/v1/search"
     }
     
     var path: String {
         switch self {
-        case .searchMembers:
-            return "/members"
+        case let .search(type, keyword):
+            return type.path
         }
     }
     
     var queryParameters: [String : String]? {
         switch self {
-        case .searchMembers(let request):
+        case .search(_, let request):
             return [
                 "keyword": "\(request.keyword)",
                 "page": "\(request.pageNumber)",
@@ -37,35 +55,35 @@ extension SearchAPI: EndPoint {
     
     var method: Alamofire.HTTPMethod {
         switch self {
-        case .searchMembers:
+        case .search:
             return .get
         }
     }
     
     var headers: Alamofire.HTTPHeaders? {
         switch self {
-        case .searchMembers:
+        case .search:
             return nil
         }
     }
     
     var parameterEncoding: any Alamofire.ParameterEncoding {
         switch self {
-        case .searchMembers:
+        case .search:
             return URLEncoding.default
         }
     }
     
     var bodyParameters: Alamofire.Parameters? {
         switch self {
-        case .searchMembers:
+        case .search:
             return nil
         }
     }
     
     var requiresAuth: Bool {
         switch self {
-        case .searchMembers:
+        case .search:
             return true
         }
     }
