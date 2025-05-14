343dc10728f48cc7e35ce4cbb40034ef36cde5c8
Seongmin Kim <sjybext@naver.com>
2025-05-14 14:41:32 +0900

    [TJFE-175] design: 탐험하기 UI 수정 (#52)
    * [TJFE-175] chore: 플레이스 관련 기능 제거
    
    * [TJFE-175] design: 플레이스 관련 UI 제거
    
    * [TJFE-175] design: 탐험하기 Journal Cell UI 수정

--- TravelJournalForEveryone/TravelJournalForEveryone/Presentation/Explore/ExploreViewModel.swift
+++ TravelJournalForEveryone/TravelJournalForEveryone/Presentation/Explore/ExploreViewModel.swift
@@ -14,49 +14,27 @@ final class ExploreViewModel: ObservableObject {
     @MainActor // TEST
     func send(_ intent: Intent) {
         switch intent {
-        case .selectSegment(let index):
-            self.state.selectedSegmentIndex = index
-            
         case .journalListViewOnAppear:
             fetchJournals()
         case .journalListNextPageOnAppear:
             fetchJournals()
         case .refreshJournals:
             refreshJournals()
-            
-        case .placeGridViewOnAppear:
-            fetchPlaces()
-        case .placeGridNextPageOnAppear:
-            fetchPlaces()
-        case .refreshPlaces:
-            refreshPlaces()
         }
     }
 }
 
 extension ExploreViewModel {
     struct State {
-        var selectedSegmentIndex: Int = 0
-        
         var journalSummaries: [ExploreJournalSummary] = []
         var isJournalsInitialLoading: Bool = true
         var isLastJournalsPage: Bool = false
-        
-        var placesSummaries: [PlaceSummary] = []
-        var isPlacesInitialLoading: Bool = true
-        var isLastPlacesPage: Bool = false
     }
     
     enum Intent {
-        case selectSegment(Int)
-        
         case journalListViewOnAppear
         case journalListNextPageOnAppear
         case refreshJournals
-        
-        case placeGridViewOnAppear
-        case placeGridNextPageOnAppear
-        case refreshPlaces
     }
 }
 
@@ -80,33 +58,5 @@ extension ExploreViewModel {
     
     private func refreshJournals() {
         print("여행 일지 목록 새로 고침")
-    }
-    
-    @MainActor // TEST
-    private func fetchPlaces() {
-        // TEST
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            self.state.isPlacesInitialLoading = false
-            self.state.isLastPlacesPage = true
-            
-            self.state.placesSummaries = [
-                .mock(id: 0, placeName: "시흥"),
-                .mock(id: 1, placeName: "주문진"),
-                .mock(id: 2, placeName: "전주"),
-                .mock(id: 3, placeName: "인덕원"),
-                .mock(id: 4, placeName: "잠실"),
-                .mock(id: 5, placeName: "부천"),
-                .mock(id: 6, placeName: "광명"),
-                .mock(id: 7, placeName: "제주"),
-                .mock(id: 8, placeName: "부산"),
-                .mock(id: 9, placeName: "강릉"),
-                .mock(id: 10, placeName: "광진구"),
-                .mock(id: 11, placeName: "해운대구"),
-            ]
-        }
-    }
-    
-    private func refreshPlaces() {
-        print("플레이스 목록 새로 고침")
     }
 }
